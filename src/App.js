7a0cd97de74623e2edaa504e9b89ea45a9a059dd
ezgikardes <ezgikardes@gmail.com>
2024-01-17 19:34:32 +0100

    görev tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,59 +1,61 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components'
+import Form from './components/Form';
 
-import Form from "./components/Form";
 
 function App() {
 
-  //Üyeleri ve düzenleme modunu saklamak için state oluşturduk.
-  const [members, setMembers] = useState([]);
-  const [editMode, setEditMode] = useState(null);
+  //Üyeleri tutan bir state oluşturduk.
+  const [memberList, setMemberList] = useState([]);
 
-  //Yeni üye eklemek için bir işlev tanımladık.
+  //Editlenen üyeleri tutan bir state oluşturduk.
+const [editedMember, setEditedMember] = useState();
+
+
+
+  //editleme için bir fonksiyon yazalım. memberList'i maplesin, içindeki belirli bir id'ye sahip olan üyenin güncellenmiş bir versiyonuyla değiştirilmesini sağlasın. Eğer member.id ile mevcut üyenin (m) id'si eşleşirse, o üye güncellenir; aksi takdirde, mevcut üye olduğu gibi korunur. sonra bu fonksiyonu Form component'ine props olarak gönderelim.
+  const editingMember = (member) => {
+    setMemberList(
+      memberList.map(m => {
+      if (member.id === m.id){
+        return member;
+      } else {
+        return m;
+      }
+    })
+    )  
+  }; 
+
+  //Yeni üye eklemek için bir callback fonksiyonu oluşturduk. Bunu Form component'ine props olarak göndereceğiz ki, submit butonuna tıklandığında bu fonksiyonu tetiklesin.
   const addMember = (newMember) => {
-    if (editMode) {
-      const updatedMembers = members.map((member) =>
-        member.id === editMode.id 
-          ? newMember
-          : member
-      );
-      setMembers(updatedMembers);
-      setEditMode(null);
-    } else {
-      setMembers([...members, newMember]);
-    }
+    setMemberList([...memberList, newMember]); //mevcut memberList'i al, ona yeni gelen üyeyi ekleyip, state'i güncelle.
+    console.log("new member data > ", newMember); 
   };
 
-  const WrapperForm = styled.form`
-    display: block;
-    margin-bottom: 10px;
-  `
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <WrapperForm onSubmit={addMember}>
-          <div>
-            <Form
-              addMember={addMember}
-              editMode={editMode}
-            />
-          </div>
-        </WrapperForm>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* header ve member-card class'ı alan bir div oluşturuyoruz, oluşturacağımız tablonun sütun başlıkları için: */}
+        <div className='member-card header'>
+        <div>İsim </div>
+        <div>Email </div>
+        <div>Rol </div>
+        <div>Aksiyon </div>
+      </div> 
+    {memberList.map((member) => (//memberList'i map'ledik, her bir member için, o member'ın ismi, emaili ve rolünü(yani formData state'ine gelen  dataları) dinamik bir şekilde render eden bir div yazalım: 
+      <div className='member-card'>
+        <div>{member.name}</div>
+        <div>{member.email}</div>
+        <div>{member.role}</div>
+        <div>
+          <button onClick= {() => setEditedMember(member)}>Düzenle</button> 
+        </div>
+      </div> 
+    ))} 
+    <Form addMember={addMember} editingMember={editingMember} editedMember={editedMember} />
     </div>
   );
-}
+  }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,129 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-
-const FormContainer = styled.div`
-  text-align: center;
-  max-width: 300px;
-  margin: 0 auto;
-`;
-
-const FormGroup = styled.div`
-  margin-bottom: 15px;
-`;
-
-const FormLabel = styled.label`
-  display: block;
-  font-weight: bold;
-`;
-
-const FormButton = styled.button`
-  display: block;
-  background-color: #007BFF;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  margin-top: 10px;
-  margin: 10px auto;
-  cursor: pointer;
-`;
+import { useEffect, useState } from "react";
 
 
-function Form(props) {
 
-    const [formData, setFormData] = useState({ //form verilerini saklayan state
-        name: "", 
+
+
+export default function Form({ addMember, editingMember, editedMember }) {
+  const [memberData, setMemberData] = useState({
+    name: "",
+    email: "",
+    role: "",
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault(); //form submit edildiğinde default davranış olan sayfayı yenilemeyi engelle.
+    if (memberData.id) { 
+        editingMember(memberData); //bu koşulu edit için son aşamada ekliyoruz. Eğer memberData.id varsa, yani zaten bir üye düzenleniyorsa, editingMember çalışmalı.
+    } else { //değilse yeni üye eklenmeli, addMember çalışmalı.
+        addMember({...memberData, id: Math.round(Math.random() * 9999999)}); //submit ettiğimde, form için tuttuğumuz datayı (memberData), yeni bir id vererek addMember prop'u aracılığıyla App'teki aynı isimli callback fonksiyonuna gönder.
+    }
+    setMemberData({
+        name: "",
         email: "",
         role: "",
-    }); 
-    const [isEditing, setIsEditing] = useState(false); //Üye düzenleme modunu izleyen state
-
-    useEffect(() => {
-        props.editMode
-            ? setFormData(props.editMode) 
-            : setFormData({ 
-                name: "", 
-                email: "",
-                role: "",
-            })
-        props.editMode 
-            ? setIsEditing(true)
-            : setIsEditing(false);
-    }, [props.editMode])
-
-    const submitHandler = (e) => { //Form verilerini, props üzerinden ana bileşen bir işlev yarattık
-        e.preventDefault();
-        props.addMemmber(formData); // Form verilerini props üzerinden ana bileşene ilet
-        setIsEditing(false); //Düzenleme modunu kapat
-        setFormData({
-            name: "",
-            email: "",
-            role: "",
-          }); //Form verilerini sıfırla
-    };
-
-    // Kullanıcının her bir input alanına girdiği verileri 'formData' içinde güncelleyecek bir işlev:
-    const changeHandler = (e) => { 
-        const newFormData = {
-            ...formData,
-            [e.target.name]: e.target.value,
-        };
-        setFormData(newFormData); // Kullanıcının girdiği verileri form verilerine ekle
-    }
+      }) //submit ettiğimde formData'yı sıfırla. ama bunun çalışması için aşağıda inputları controlled hale getirdik. value propuna state'i bağladık.
+  };
 
 
-    return(
-        <div>
-            <FormContainer>
-            {isEditing ? <h2>Üye Düzenle</h2> : <h2>Yeni Üye Ekle</h2>}
+  useEffect(() => {
+    if(editedMember){
+        setMemberData(editedMember);
+    }}, [editedMember]) //editedMember state'i değiştiğinde, yani bir edit butonuna basıldıysa, bu useEffect çalıştırılacak. Eğer editedMember'ın içinde bir data varsa, bu datayı memberData'nın içerisine setle dedik.  böylece, düzenle butonuna basıldığında, söz konusu data inputların içine dolacak.
 
-            <form onSubmit={submitHandler}>
 
-                <FormGroup>
-                <FormLabel htmlFor="name">İsim </FormLabel>
-                <input
-                    onChange={(e) => changeHandler(e)}
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    />
-                </FormGroup>
-                
-                <FormGroup>
-                <FormLabel htmlFor="email">Eposta</FormLabel>
-                <input
-                    onChange={(e) => changeHandler(e)}
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                />
-                </FormGroup>
+  return (
+    <form onSubmit={submitHandler} className="member-form">
+    <h2>
+        {memberData.id ? `${memberData.name} kaydını güncelle` : "Yeni kayıt oluştur"} 
+    </h2> 
+    <hr style={{marginBottom:"1.5rem"}} />
+      <div>
+        <label>
+          <span>
+            Üye adı:
+          </span>
+          <input
+            type="text"
+            onChange={(e) => {
+              setMemberData({ ...memberData, name: e.target.value });
+            }} //input'taki değişikliği state'e yaz dedik.
+            value={memberData.name} //input'un value prop'unu, memberData state'inin name alanına eşitledik, state'teki  değişikliği inputa yaz dedik. 
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+           <span>
+            Üye email:
+           </span>
+          <input
+            type="email"
+            onChange={(e) => {
+              setMemberData({ ...memberData, email: e.target.value });
+            }}
+            value={memberData.email} 
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+           <span>
+            Üye rolü:
+           </span>
+          <input
+            type="text"
+            onChange={(e) => {
+              setMemberData({ ...memberData, role: e.target.value });
+            }}
+            value={memberData.role} 
+          />
+        </label>
+      </div>
 
-                <FormGroup>
-                <FormLabel htmlFor="role">Rol</FormLabel>
-                <input
-                    onChange={(e) => changeHandler(e)}
-                    type="text"
-                    name="role"
-                    value={formData.role}
-                />
-                </FormGroup>
+      <button type="submit" disabled={!(memberData.name && memberData.email)}>Gönder</button>
+      {/* bu buton, tıklandığında yukarıdaki submitHandler vesilesiyle App'e giderek addMember callback fonksiyonunu tetikleyecek, memberList state'i güncellenecek ve map'le yeniden render edilecek. */}
+    </form>
+  );
+}
 
-                <FormGroup>
-                <FormButton type="submit">
-                    {isEditing ? "Üye Düzenle" : "Yeni Üye Ekle"}
-                </FormButton>
-                </FormGroup>
+//not: return kısmında bir form döndürdüğümüz için, button'a onClick eklememize gerek kalmıyor. type'ına submit vermemiz, tıkladığımızda form'un onSubmit'inin çalışması için yeterli.
 
-                <FormGroup>
-                <FormButton type="button" onClick={() => setFormData({name: "", email: "", role: "",})}>
-                Sıfırla
-                </FormButton>
-                </FormGroup>
+//not: 19. satırdaki kodu, edit fonksiyonunda kullanmak üzere, bir id yaratmak için en son aşamada ekledik. başkasıyla çakışmayacak eşsiz bir id için math.random() metdunu kullandık.
 
-            </form>
-            </FormContainer>
-        </div>
-    );
-};
+//not: 80. satırda buton'daki disabled propu ekledik. disable'ı, input'lardaki name ve email yoksa true yap yani aktif et dedik. varsa false yap. 
 
-export default Form;
+//not: 37. satırda, 
